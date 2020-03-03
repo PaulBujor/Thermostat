bab4371e879f393f992775e2d29c4fe9710e24b1
Ranteidz <55162517+Ranteidz@users.noreply.github.com>
2020-03-03 10:09:55 +0100

    Heater state init

---
+++ Project/src/main/java/model/HeaterPosition1.java
@@ -0,0 +1,12 @@
+package model;
+
+public class HeaterPosition1 extends HeaterState
+{
+
+  public HeaterPosition1(Heater heater){
+
+  }
+  public void up(Heater heater){
+    heater.setState(new );
+  }
+}
---
+++ Project/src/main/java/model/HeaterState.java
@@ -0,0 +1,12 @@
+package model;
+
+public abstract class HeaterState
+{
+public void up(Heater heater){}
+public void down(Heater heater){}
+public String status(){
+  return null;
+
+}
+
+}
