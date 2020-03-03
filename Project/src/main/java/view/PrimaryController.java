44b7b20b27acc4d5711845ded5aa22a9d859864e
glad1o <2000Rulet?isabadoass>
2020-03-03 12:47:22 +0100

    viewmodel + controller

--- Project/src/main/java/view/PrimaryController.java
+++ Project/src/main/java/view/PrimaryController.java
@@ -1,10 +1,100 @@
 package view;
 
 import java.io.IOException;
+
+import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Region;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import viewmodel.MainViewModel;
 
 
 public class PrimaryController {
+
+    @FXML
+    private Circle t0Circle;
+
+    @FXML
+    private Circle t1Circle;
+
+    @FXML
+    private Circle t2Circle;
+
+    @FXML
+    private Label valueT0;
+
+    @FXML
+    private Label valueT1;
+
+    @FXML
+    private Label valueT2;
+
+    @FXML
+    private Button increaseButton;
+
+    @FXML
+    private Button decreaseButton;
+
+    @FXML
+    private Label powerPosition;
+
+    private Region root;
+/*    private ViewHandler viewHandler;*/
+    private MainViewModel viewModel;
+
+
+
+    public void init() {
+        Bindings.bindBidirectional(valueT0.textProperty(), viewModel.t0Property(), new StringIntegerConverter(0));
+        Bindings.bindBidirectional(valueT1.textProperty(), viewModel.t1Property(), new StringIntegerConverter(0));
+        Bindings.bindBidirectional(valueT2.textProperty(), viewModel.t2Property(), new StringIntegerConverter(0));
+        Bindings.bindBidirectional(powerPosition.textProperty(), viewModel.heaterStateProperty(), new StringIntegerConverter(0));
+
+        increaseButton.setOnAction((evt) -> viewModel.increaseTemperature());
+        decreaseButton.setOnAction((evt) -> viewModel.decreaseTemperature());
+
+
+        if (viewModel.t0Property().get() <= 0)
+        {
+            t0Circle.setStroke(Color.rgb(31, 154, 255));
+            t0Circle.setOpacity(viewModel.t0Property().get() / 20);
+        }
+        else
+        {
+            t0Circle.setStroke(Color.rgb(255,33,33));
+            t0Circle.setOpacity(viewModel.t0Property().get() / 20);
+        }
+
+        if (viewModel.t1Property().get() <= 0)
+        {
+            t1Circle.setStroke(Color.rgb(31, 154, 255));
+            t1Circle.setOpacity(viewModel.t1Property().get() / 20);
+        }
+        else
+        {
+            t1Circle.setStroke(Color.rgb(255,33,33));
+            t1Circle.setOpacity(viewModel.t1Property().get() / 20);
+        }
+
+        if (viewModel.t2Property().get() <= 0)
+        {
+            t2Circle.setStroke(Color.rgb(31, 154, 255));
+            t2Circle.setOpacity(viewModel.t2Property().get() / 20);
+        }
+        else
+        {
+            t2Circle.setStroke(Color.rgb(255,33,33));
+            t2Circle.setOpacity(viewModel.t2Property().get() / 20);
+        }
+
+
+
+    }
+
+
 
     @FXML
     private void switchToSecondary() throws IOException {
--- Project/src/main/java/viewmodel/MainViewModel.java
+++ Project/src/main/java/viewmodel/MainViewModel.java
@@ -1,4 +1,76 @@
 package viewmodel;
 
-public class MainViewModel {
+import javafx.application.Platform;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import model.HeaterModel;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class MainViewModel implements PropertyChangeListener {
+
+    private HeaterModel model;
+    private DoubleProperty t0;
+    private DoubleProperty t1;
+    private DoubleProperty t2;
+    private IntegerProperty heaterState;
+
+    public MainViewModel(HeaterModel model)
+    {
+        this.model = model;
+        t0 = new SimpleDoubleProperty();
+        t1 = new SimpleDoubleProperty();
+        t2 = new SimpleDoubleProperty();
+        heaterState = new SimpleIntegerProperty();
+        model.addListener("t0", this);
+        model.addListener("t1", this);
+        model.addListener("t2", this);
+        model.addListener("heater", this);
+    }
+
+
+    public DoubleProperty t0Property() {
+        return t0;
+    }
+
+    public DoubleProperty t1Property() {
+        return t1;
+    }
+
+    public DoubleProperty t2Property() {
+        return t2;
+    }
+
+    public IntegerProperty heaterStateProperty() {
+        return heaterState;
+    }
+
+    public void increaseTemperature()
+    {
+        model.heaterUp();
+    }
+
+    public void decreaseTemperature()
+    {
+        model.heaterDown();
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        switch (evt.getPropertyName())
+        {
+            case "t0": t0.setValue(Double.parseDouble((String) evt.getNewValue()));
+            break;
+            case "t1": t1.setValue(Double.parseDouble((String) evt.getNewValue()));
+            break;
+            case "t2": t2.setValue(Double.parseDouble((String) evt.getNewValue()));
+            break;
+            case "heater": heaterState.setValue(Double.parseDouble((String) evt.getNewValue()));
+            break;
+        }
+
+    }
 }
