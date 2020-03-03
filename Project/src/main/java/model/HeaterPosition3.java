9653ada2dec53a19d60c78c7592716591552715c
Ranteidz <55162517+Ranteidz@users.noreply.github.com>
2020-03-03 13:34:32 +0100

    Its completely ******

--- Project/src/main/java/model/HeaterPosition3.java
+++ Project/src/main/java/model/HeaterPosition3.java
@@ -40,37 +40,4 @@ if(!completed){
     return 3;
   }
 }
-/*
-public class DoorOpen extends DoorState {
-  private Thread timeout;
-  private boolean completed = false;
 
-  public DoorOpen(Door door) {
-    timeout = new Thread(() -> {
-      try {
-        Thread.sleep(5000);
-        timeout(door);
-      } catch (InterruptedException e) {
-        System.out.println("Timeout interrupted");
-      }
-    });
-    timeout.start();
-  }
-
-  @Override
-  public synchronized void click(Door door) {
-    if (!completed) {
-      completed = true;
-      timeout.interrupt();
-      door.setState(new DoorStayOpen());
-    }
-  }
-
-  public synchronized void timeout(Door door) {
-    if (!completed) {
-      completed = true;
-      door.setState(new DoorClosing(door));
-    }
-  }
-}
-*/
--- Project/src/main/java/model/InternalThermometer.java
+++ Project/src/main/java/model/InternalThermometer.java
@@ -3,60 +3,73 @@ package model;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 
-public class InternalThermometer implements Runnable, PropertyChangeSubject {
-    private double outTemp;
-    private double lastTemp = 10;
-    private int distance;
-    private int heaterMode = 0;
-    private PropertyChangeSupport property;
-    private String thermoId;
+public class InternalThermometer implements Runnable, PropertyChangeSubject
+{
+  private double outTemp;
+  private double lastTemp = 10;
+  private int distance;
+  private int heaterMode = 0;
+  private PropertyChangeSupport property;
+  private String thermoId;
 
-    public InternalThermometer(String thermoId, int distance) {
-        property = new PropertyChangeSupport(this);
-        this.thermoId = thermoId;
-        this.distance = distance;
-    }
+  public InternalThermometer(String thermoId, int distance)
+  {
+    property = new PropertyChangeSupport(this);
+    this.thermoId = thermoId;
+    this.distance = distance;
+  }
 
-    public void changeHeaterMode(int mode) {
-        heaterMode = mode;
-    }
+  public void changeHeaterMode(int mode)
+  {
+    heaterMode = mode;
+  }
 
-    @Override
-    public void run() {
-        while (true) {
-            property.firePropertyChange(thermoId, lastTemp, temperature(lastTemp, heaterMode, distance, outTemp, 6));
-            try {
-                Thread.sleep(6000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+  @Override public void run()
+  {
+    while (true)
+    {
+      property.firePropertyChange(thermoId, lastTemp,
+          temperature(lastTemp, heaterMode, distance, outTemp, 6));
+      try
+      {
+        Thread.sleep(6000);
+      }
+      catch (InterruptedException e)
+      {
+        e.printStackTrace();
+      }
     }
+  }
 
-    public double temperature(double t, int p, int d, double t0, int s) {
-        double tMax = Math.min(11 * p + 10, 11 * p + 10 + t0);
-        tMax = Math.max(Math.max(t, tMax), t0);
-        double heaterTerm = 0;
-        if (p > 0) {
-            double den = Math.max((tMax * (20 - 5 * p) * (d + 5)), 0.1);
-            heaterTerm = 30 * s * Math.abs(tMax - t) / den;
-        }
-        double outdoorTerm = (t - t0) * s / 250.0;
-        t = Math.min(Math.max(t - outdoorTerm + heaterTerm, t0), tMax);
-        return t;
+  public double temperature(double t, int p, int d, double t0, int s)
+  {
+    double tMax = Math.min(11 * p + 10, 11 * p + 10 + t0);
+    tMax = Math.max(Math.max(t, tMax), t0);
+    double heaterTerm = 0;
+    if (p > 0)
+    {
+      double den = Math.max((tMax * (20 - 5 * p) * (d + 5)), 0.1);
+      heaterTerm = 30 * s * Math.abs(tMax - t) / den;
     }
+    double outdoorTerm = (t - t0) * s / 250.0;
+    t = Math.min(Math.max(t - outdoorTerm + heaterTerm, t0), tMax);
+    return t;
+  }
 
-    public void updateOutTemp(double outTemp) {
-        this.outTemp = outTemp;
-    }
+  public void updateOutTemp(double outTemp)
+  {
+    this.outTemp = outTemp;
+  }
 
-    @Override
-    public void addListener(String evtid, PropertyChangeListener listener) {
-        property.addPropertyChangeListener(evtid, listener);
-    }
+  @Override public void addListener(String evtid,
+      PropertyChangeListener listener)
+  {
+    property.addPropertyChangeListener(evtid, listener);
+  }
 
-    @Override
-    public void removeListener(String evtid, PropertyChangeListener listener) {
-        property.removePropertyChangeListener(evtid, listener);
-    }
+  @Override public void removeListener(String evtid,
+      PropertyChangeListener listener)
+  {
+    property.removePropertyChangeListener(evtid, listener);
+  }
 }
