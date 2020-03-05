2d2d3c02270816202381011172197b079b0a405f
Ranteidz <55162517+Ranteidz@users.noreply.github.com>
2020-03-05 14:22:27 +0100

    secondary controller  + fxml + tableData

--- Project/src/main/java/view/SecondaryController.java
+++ Project/src/main/java/view/SecondaryController.java
@@ -2,8 +2,15 @@ package view;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
 
 public class SecondaryController {
+
+   @FXML Button SwithToMainButton;
+@FXML TableView<TableRowData> thermometerTable;
+@FXML
+
 
     @FXML
     private void switchToPrimary() throws IOException {
---
+++ Project/src/main/java/view/TableRowData.java
@@ -0,0 +1,15 @@
+package view;
+
+import javafx.beans.property.DoubleProperty;
+
+public class TableRowData
+{
+private DoubleProperty thermometer0;
+private DoubleProperty thermometer1;
+private DoubleProperty thermometer2;
+
+
+public TableRowData(){
+
+}
+}
